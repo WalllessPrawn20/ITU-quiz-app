f6261420e1b4b02665ea3bfad5bcf804910a5ac5
xmelchm00 <xmelchm00@vutbr.cz>
2025-12-13 19:41:29 +0100

    comments in js

--- src/utils/AudioManager.js
+++ src/utils/AudioManager.js
@@ -1,6 +1,11 @@
-// src/utils/audioManager.js
+////////////////////////////////
+//// Author: Matej Melchiory ///
+//// Login: xmelchm00 //////////
+//// Date: 9.12.2025 ///////////
+////////////////////////////////
 class AudioManager {
   constructor() {
+    // Preload audio tracks
     this.tracks = {
       synthwave: new Audio(new URL('./synthwave.mp3', import.meta.url).href),
       chill: new Audio(new URL('./chill.mp3', import.meta.url).href),
@@ -12,22 +17,28 @@ class AudioManager {
     this.current.loop = true
     this.current.volume = 0.5
 
+    // Enable from the start
     this.enabled = true
   }
+
+  // Playing music
   play() {
     if (!this.current) return
     this.current.play()
   }
 
+  // Pausing music
   pause() {
     this.current.pause()
   }
 
+  // Toggle music
   toggle() {
     this.enabled = !this.enabled
     this.enabled ? this.current.play() : this.current.pause()
   }
 
+  // Switch track to the new chosen
   switchTrack(name) {
     if (!this.tracks[name]) return
     if (this.current === this.tracks[name]) return
@@ -41,6 +52,7 @@ class AudioManager {
     if (this.enabled && wasPlaying) this.current.play()
   }
 
+  // Set volume
   setVolume(v) {
     this.current.volume = v
   }
--- src/utils/SfxManager.js
+++ src/utils/SfxManager.js
@@ -1,7 +1,14 @@
+////////////////////////////////
+//// Author: Matej Melchiory ///
+//// Login: xmelchm00 //////////
+//// Date: 9.12.2025 ///////////
+////////////////////////////////
+
 import clickSfx from './button.mp3'
 
 class SfxManager {
   constructor() {
+    // Enable sound effects from the start
     this.enabled = true
     this.click = new Audio(clickSfx)
     this.click.volume = 0.5
@@ -12,11 +19,13 @@ class SfxManager {
     }
   }
 
+  // Enable or disable sound effects
   setEnabled(val) {
     this.enabled = val
     sessionStorage.setItem('sfx', val)
   }
 
+  // Play click sound effect
   playClick() {
     if (!this.enabled) return
     this.click.currentTime = 0
